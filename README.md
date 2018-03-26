58635d44b43cfaf527f455da463b273560f1eb30
John C. Hawkins <jchawkins@vhpads.com>
2018-03-26 14:00:15 -0500

    refactored all code to put all HEX types into a single struct.

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# ihex
-Golang library for reading and writing Intel HEX files
+# IHEX - Go reader / writer for Intel HEX files
+
+Library for reading and writing Intel HEX files according to the specification outlined in [Wikipedia's Article](https://en.wikipedia.org/wiki/Intel_HEX).
+
+## Features
+
+* Reading and Writing HEX files
+* HEX Record Types
+* Automatic HEX record checksum creation / validation
+* Automatic HEX record type validation against HEX file type specifications
+* Supports I8HEX, I16HEX, and I32HEX file specifications
+
+## Installation
+
+Add the littlehawk93/ihex package to your project using the standard go import method:
+
+    go get github.com/littlehawk93/ihex
+
+Once installed, include the package in your project with the following import command:
+
+    import "github.com/littlehawk93/ihex"
+
+## Examples
+
+To read an Intel HEX file in the I8HEX specification:
+
+    file, err := os.Open("/path/to/hex/file.hex")
+
+    if err == nil {
+        
+        hexFile := ihex.NewHexFile()
+
+        hexFile.Type = ihex.HexFileTypeI8HEX
+
+        err := hexFile.ReadFrom(file)
+
+        if err == nil {
+
+            // Use hexFile here
+        }
+    }
+
+Once parsed, a HEX file struct can be read programmatically to get data from it:
+
+    // Iterate over all records in HEX file
+    for hexFile.Next() {
+
+        record := hexFile.Record()
+
+        type := record.RecordType
+        address := record.AddressOffset
+
+        data := record.Data
+
+        // Do stuff
+    }
+
+You can begin reading a HEX file from the beginning again at any time by calling Reset():
+
+    hexFile.Reset()
+
+You can also programmatically construct your own HEX file by adding records:
+
+    hexFile := ihex.NewHexFile()
+    hexFile.Type = ihex.HexFileTypeI16HEX
+
+    var record ihex.Record
+
+    record.RecordType = ihex.RecordTypeEndOfFile
+    record.AddressOffset = 0
+    record.Data = make([]byte, 0)
+
+    hexFile.AddRecord(&record)
+
+HEX file structs can be written back to Intel HEX file specification to any writer interface like so:
+
+    file, err := os.Create("/path/to/hex/file.hex")
+
+    if err == nil {
+        
+        err = hexFile.WriteTo(file)
+
+        // Error handling
+    }
--- ihex.go
+++ ihex.go
@@ -1,3 +1,4 @@
+// Package ihex provides a library of structs for reading and writing Intel HEX files
 package ihex
 
 import (
@@ -13,46 +14,44 @@ import (
 )
 
 const (
-	i8HEXMaxAddress  = 65535
-	i16HEXMaxAddress = 1048575
-	i32HEXMaxAddress = 4294967295
 	maxRecordLen     = 521
 	recordStartCode  = 58
 
-	// RecordTypeData - Record containing data
+	// HexFileTypeI8HEX the I8HEX file specification
+	HexFileTypeI8HEX = 8,
+
+	// HexFileTypeI16HEX the I16HEX file specification
+	HexFileTypeI16HEX = 16,
+
+	// HexFileTypeI16HEX the I32HEX file specification
+	HexFileTypeI32HEX = 32,
+
+	// RecordTypeData type of record that contains data.
 	RecordTypeData = 0
 
-	// RecordTypeEndOfFile - Record indicating the end of the Intel HEX file
+	// RecordTypeEndOfFile type of record that indicates the end of the Intel HEX file.
 	RecordTypeEndOfFile = 1
 
-	// RecordTypeExtendedSegmentAddress - Useable by I16HEX files only. Record containing a 16 bit segment base address. Multiple this record value by 16 and add to the address of all subsequent records
+	// RecordTypeExtendedSegmentAddress type of record that contains a 16 bit segment base address. 
+	// Useable by I16HEX files only. 
+	// Multiply this record value by 16 and add to the address of all subsequent records.
 	RecordTypeExtendedSegmentAddress = 2
 
-	// RecordTypeStartSegmentAddress - Useable by I16HEX files only. Initializes content of CS:IP registers for 80x86 processors
+	// RecordTypeStartSegmentAddress type of record that Initializes content of CS:IP registers for 80x86 processors.
+	// Useable by I16HEX files only. 
 	RecordTypeStartSegmentAddress = 3
 
-	// RecordTypeExtendedLinearAddress - Useable by I32HEX files only. Record containing a 16 bit segment base address. Use as the higher 16 bits of subsequent record addresses to form 32 bit addresses
+	// RecordTypeExtendedLinearAddress type of record that contains a 16 bit segment base address. 
+	// Useable by I32HEX files only. 
+	// Use as the higher 16 bits of subsequent record addresses to form 32 bit addresses.
 	RecordTypeExtendedLinearAddress = 4
 
-	// RecordTypeStartLinearAddress - Useable by I32HEX files only. Stores the 32 bit value of the EIP register of the 80386 and higher Intel CPU
+	// RecordTypeStartLinearAddress type of record that stores the 32 bit value of the EIP register of the 80386 and higher Intel CPU.
+	// Useable by I32HEX files only.
 	RecordTypeStartLinearAddress = 5
 )
 
-// File - Represents any of the file types of the Intel HEX file specification
-type File interface {
-	WriteFile(*os.File) error
-	NextRecord() *Record
-	AddRecord(*Record)
-	Size() int
-	Reset()
-}
-
-type recordOffsetIndex struct {
-	AddressOffset uint32
-	RecordIndex   int
-}
-
-// Record - Represents a single data record in an Intel HEX file
+// Record represents a single data record in an Intel HEX file.
 type Record struct {
 	RecordType    byte
 	AddressOffset uint16
@@ -63,75 +62,193 @@ func (me *Record) write(writer io.Writer) error {
 
 	buff := bytes.NewBufferString(":")
 
-	_, err := buff.WriteString(fmt.Sprintf("%02X", len(me.Data)))
+	err := me.writeNoErr(buff, fmt.Sprintf("%02X", len(me.Data)), nil)
 
-	if err != nil {
-		return err
-	}
+	err = me.writeNoErr(buff, fmt.Sprintf("%04X", me.AddressOffset), err)
 
-	_, err = buff.WriteString(fmt.Sprintf("%04X", me.AddressOffset))
-
-	if err != nil {
-		return err
-	}
-
-	_, err = buff.WriteString(fmt.Sprintf("%02X", me.RecordType))
-
-	if err != nil {
-		return err
-	}
+	err = me.writeNoErr(buff, fmt.Sprintf("%02X", me.RecordType), err)
 
 	hexBytes := make([]byte, len(me.Data)*2)
-
 	hex.Encode(hexBytes, me.Data)
 
-	_, err = buff.WriteString(strings.ToUpper(string(hexBytes)))
+	err = me.writeNoErr(buff, strings.ToUpper(string(hexBytes)), err)
+
+	err = me.writeNoErr(buff, fmt.Sprintf("%02X", generateChecksum(me.Data, me.RecordType)), err)
+
+	err = me.writeNoErr(buff, "\n", err)
 
 	if err != nil {
-		return err
+		err = writer.Write(buff.Bytes())
 	}
-
-	_, err = buff.WriteString(fmt.Sprintf("%02X", generateChecksum(me.Data, me.RecordType)))
-
-	if err != nil {
-		return err
-	}
-
-	_, err = buff.WriteString("\n")
-
-	_, err = writer.Write(buff.Bytes())
 
 	return err
 }
 
-// I8HEX - The I8HEX subfile type of the Intel HEX specification
-type I8HEX struct {
+func (me *Record) writeNoErr(buff *bytes.Buffer, str string, err error) error {
+
+	if err != nil {
+		return err
+	}
+
+	_, err = buff.WriteString(str)
+
+	return err
+}
+
+func (me *Record) read(scanner *bufio.Scanner) error {
+
+	bytes := scanner.Bytes()
+
+	if len(bytes) > maxRecordLen {
+		return fmt.Errorf("Record size (%d) than maximum record size (%d)", len(bytes), maxRecordLen)
+	}
+
+	if bytes[0] != recordStartCode {
+		return errors.New("Invalid start code for record")
+	}
+
+	byteCount, err := strconv.ParseInt(string(bytes[1:3]), 16, 8)
+
+	if err != nil {
+		return fmt.Errorf("Error parsing Byte Count: %s", err.Error())
+	}
+
+	actualByteCount := (len(bytes) - 11) / 2
+
+	if byteCount > 255 {
+		return errors.New("Invalid Byte Count provided")
+	} else if int(byteCount) != actualByteCount {
+		return fmt.Errorf("Record Byte Count (%d) does not match actual byte count (%d)", byteCount, actualByteCount)
+	}
+
+	me.Data = make([]byte, int(byteCount))
+
+	address, err := strconv.ParseUint(string(bytes[3:7]), 16, 16)
+
+	if err != nil {
+		return fmt.Errorf("Error parsing Address: %s", err.Error())
+	}
+
+	me.AddressOffset = uint16(address)
+
+	recordType, err := strconv.ParseUint(string(bytes[7:9]), 16, 8)
+
+	if err != nil {
+		return fmt.Errorf("Error parsing Record Type: %s", err.Error())
+	}
+
+	me.RecordType = byte(recordType)
+
+	decodedBytes, err := hex.Decode(me.Data, bytes[9:bytesLen-2])
+
+	if err != nil {
+		return fmt.Errorf("Unable to parse record data: %s", err.Error())
+	}
+
+	if decodedBytes != len(me.Data) {
+		return errors.New("Unable to parse record data: Incorrect number of bytes parsed")
+	}
+
+	recordChecksum, err := strconv.ParseUint(string(bytes[bytesLen-2:bytesLen]), 16, 8)
+
+	if err != nil {
+		return fmt.Errorf("Error parsing Checksum: %s", err.Error())
+	}
+
+	actualChecksum := generateChecksum(me.Data, me.RecordType)
+
+	if byte(recordChecksum) != actualChecksum {
+		return errors.New("Record checksum doesn't match computed checksum")
+	}
+
+	return nil
+}
+
+// I8HEX the I8HEX subfile type of the Intel HEX specification.
+type HexFile struct {
+	Type          byte
 	records       []Record
-	addressOffset uint16
 	recordIndex   int
 }
 
-// NextRecord - Retrieve the next HEX record from the I8HEX file. Nil if last record has already been read
-func (me *I8HEX) NextRecord() *Record {
+// Next advances the internal cursor to the next HEX record.
+// Must be called at least once before calling the Record() method to initialize the internal cursor. 
+// Returns false when there are no more records to read.
+// Otherwise, returns true.
+func (me *HexFile) Next() *Record {
 
-	if me.recordIndex >= len(me.records) {
+	return ++me.recordIndex < len(me.records)
+}
 
+// Record retrieves the HEX record at the current position of the internal cursor
+// Returns nil if the internal cursor hasn't been initialized or has moved past the last record.
+func (me *HexFile) Record() *Record {
+
+	if me.recordIndex < 0 || me.recordIndex >= len(me.records) {
 		return nil
 	}
 
-	record := me.records[me.recordIndex]
-
-	me.recordIndex++
-
-	return &record
+	return me.records[me.recordIndex]
 }
 
-// WriteFile - Write this I8HEX to a file
-func (me *I8HEX) WriteFile(file *os.File) error {
+// ReadFrom reads HEX formatted record data from a reader.
+// Returns any errors generated.
+func (me *HexFile) ReadFrom(reader io.Reader) error {
+
+	scanner := bufio.NewScanner(reader)
+
+	records := make([]Record, 0)
+
+	lineNumber := 1
+
+	for scanner.Next() {
+
+		var record Record
+
+		err := record.read(scanner)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return make([]Record, 0), fmt.Errorf("Error on line %d: '%s'", lineNumber, err.Error())
+			}
+		}
+
+		if len(records) > 0 && records[len(records) - 1].RecordType == RecordTypeEndOfFile {
+			return make([]Record, 0), fmt.Errorf("Error on line %d: 'Record parsed after EOF record'", lineNumber)
+		}
+
+		if !validateHEXRecord(record, me.Type) {
+			return make([]Record, 0), fmt.Errorf("Error on line %d: 'Invalid Record Type (%d)'", lineNumber, record.RecordType)
+		}
+
+		records = append(records, record)
+
+		lineNumber++
+	}
+
+	if len(records) == 0 || records[len(records) - 1].RecordType != RecordTypeEndOfFile {
+		return make([]Record, 0), fmt.Errorf("Error: No EOF record type parsed before EOF")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	me.records = records
+	me.Reset()
+
+	return nil
+}
+
+// Write writes this HEX file data to a writer.
+// Returns any errors generated.
+func (me *HexFile) WriteTo(writer io.Writer) error {
 
 	for _, record := range me.records {
 
-		err := record.write(file)
+		err := record.write(writer)
 
 		if err != nil {
 			return err
@@ -141,183 +258,56 @@ func (me *I8HEX) WriteFile(file *os.File) error {
 	return nil
 }
 
-// AddRecord - Add a new HEX record to this I8HEX file
-func (me *I8HEX) AddRecord(record *Record) {
+// AddRecord adds a new HEX record to this HEX file.
+func (me *HexFile) AddRecord(record *Record) error {
+
+	if !validateHEXRecord(record, me.Type) {
+		return errors.New("Invalid record type for this HEX type")
+	}
+
+	if len(me.records) > 0 && me.records[len(me.records) - 1].RecordType == RecordTypeEndOfFile {
+		return errors.New("Cannot add records after EOF record has been added")
+	}
 
 	me.records = append(me.records, *record)
+
+	return nil
 }
 
-// Size - Get the total count of records in this I8HEX file
-func (me *I8HEX) Size() int {
+// Reset moves the internal cursor to begin reading records from the beginning.
+func (me *HexFile) Reset() {
 
-	return len(me.records)
+	me.recordIndex = -1
 }
 
-// Reset - Begin reading records from this I8HEX file from the beginning record
-func (me *I8HEX) Reset() {
+// NewHexFile creates a new empty HEX file.
+func NewHexFile() *HexFile {
 
-	me.recordIndex = 0
+	var hex HexFile
+
+	hex.records = make([]Record, 0)
+	hex.Reset()
+
+	return &hex
 }
 
-// I16HEX - The I16HEX subfile type of the Intel HEX specification
-type I16HEX struct {
-	records       []Record
-	addressOffset uint32
-	recordIndex   int
-	offsetIndexes []recordOffsetIndex
-}
+func validateHEXRecord(record *Record, hexType byte) bool {
 
-// I32HEX - The I32HEX subfile type of the Intel HEX specification
-type I32HEX struct {
-	records       []Record
-	addressOffset uint32
-	recordIndex   int
-	offsetIndexes []recordOffsetIndex
-}
-
-// NewI8HEX - Create a new I8HEX file by parsing data from a reader
-func NewI8HEX(reader io.Reader) (*I8HEX, error) {
-
-	var file I8HEX
-
-	file.addressOffset = 0
-	file.recordIndex = 0
-	file.records = make([]Record, 0)
-
-	bReader := bufio.NewReader(reader)
-
-	lineNumber := 1
-
-	for true {
-
-		record, err := readRecord(bReader)
-
-		if err != nil {
-
-			if err == io.EOF {
-				break
-			} else {
-				return nil, fmt.Errorf("Error on line %d: '%s'", lineNumber, err.Error())
-			}
-		}
-
-		if record.RecordType != RecordTypeData && record.RecordType != RecordTypeEndOfFile {
-			return nil, fmt.Errorf("Error on line %d: 'Invalid Record Type (%d)'", lineNumber, record.RecordType)
-		}
-
-		file.records = append(file.records, *record)
-
-		lineNumber++
+	if record == nil {
+		return false
 	}
 
-	return &file, nil
-}
+	isDataOrEOF := record.RecordType == RecordTypeData || record.RecordType == RecordTypeEndOfFile
 
-// NewI16HEX - Create a new I16HEX file by parsing data from a reader
-func NewI16HEX(reader *io.Reader) (*I16HEX, error) {
-
-	return nil, nil
-}
-
-// NewI32HEX - Create a new I32HEX file by parsing data from a reader
-func NewI32HEX(reader *io.Reader) (*I32HEX, error) {
-
-	return nil, nil
-}
-
-func readRecord(reader *bufio.Reader) (*Record, error) {
-
-	var record Record
-
-	bytes := make([]byte, 0)
-
-	bytesLen := 0
-
-	reading := true
-
-	for reading {
-
-		tmpBytes, isPrefix, err := reader.ReadLine()
-
-		if err != nil {
-			return nil, err
-		}
-
-		bytes = append(bytes, tmpBytes[:]...)
-
-		bytesLen = len(bytes)
-
-		reading = isPrefix
-
-		if bytesLen > maxRecordLen {
-			return nil, errors.New("Maximum Record length exceeded")
-		}
+	if hexType == HexFileTypeI8HEX {
+		return isDataOrEOF
+	} else if hexType == HexFileTypeI16HEX {
+		return isDataOrEOF || record.RecordType == RecordTypeExtendedSegmentAddress || record.RecordType == RecordTypeExtendedSegmentAddress
+	} else if hexType == HexFileTypeI32HEX {
+		return isDataOrEOF || record.RecordType == RecordTypeExtendedLinearAddress || record.RecordType == RecordTypeStartLinearAddress
+	} else {
+		return false
 	}
-
-	if bytes[0] != recordStartCode {
-
-		return nil, errors.New("Invalid start code for record")
-	}
-
-	byteCount, err := strconv.ParseInt(string(bytes[1:3]), 16, 8)
-
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing Byte Count: %s", err.Error())
-	}
-
-	actualByteCount := (len(bytes) - 11) / 2
-
-	if byteCount > 255 {
-		return nil, errors.New("Invalid Byte Count provided")
-	} else if int(byteCount) != actualByteCount {
-		return nil, fmt.Errorf("Record Byte Count (%d) does not match actual byte count (%d)", byteCount, actualByteCount)
-	}
-
-	record.Data = make([]byte, int(byteCount))
-
-	address, err := strconv.ParseUint(string(bytes[3:7]), 16, 16)
-
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing Address: %s", err.Error())
-	}
-
-	record.AddressOffset = uint16(address)
-
-	recordType, err := strconv.ParseUint(string(bytes[7:9]), 16, 8)
-
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing Record Type: %s", err.Error())
-	}
-
-	if recordType != RecordTypeData && recordType != RecordTypeEndOfFile && recordType != RecordTypeExtendedLinearAddress && recordType != RecordTypeExtendedSegmentAddress && recordType != RecordTypeStartLinearAddress && recordType != RecordTypeStartSegmentAddress {
-		return nil, fmt.Errorf("Invalid Record Type (%d)", recordType)
-	}
-
-	record.RecordType = byte(recordType)
-
-	decodedBytes, err := hex.Decode(record.Data, bytes[9:bytesLen-2])
-
-	if err != nil {
-		return nil, fmt.Errorf("Unable to parse record data: %s", err.Error())
-	}
-
-	if decodedBytes != len(record.Data) {
-		return nil, errors.New("Unable to parse record data: Incorrect number of bytes parsed")
-	}
-
-	recordChecksum, err := strconv.ParseUint(string(bytes[bytesLen-2:bytesLen]), 16, 8)
-
-	if err != nil {
-		return nil, fmt.Errorf("Error parsing Checksum: %s", err.Error())
-	}
-
-	actualChecksum := generateChecksum(record.Data, record.RecordType)
-
-	if byte(recordChecksum) != actualChecksum {
-		return nil, errors.New("Record checksum doesn't match computed checksum")
-	}
-
-	return &record, nil
 }
 
 func generateChecksum(data []byte, recordType byte) byte {
